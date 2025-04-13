242b3aedf3f0dd58fea5c8c9cc71648d309e9f66
Antony Murithi <antonymurithi51@gmail.com>
2025-04-13 12:48:00 +0300

    created a single record SQL query

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -14,6 +14,6 @@ func (app *application) serverError(w http.ResponseWriter, r *http.Request, err 
 	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 }
 
-func (app *application) clientError(w http.ResponseWriter, status int) {
+func (app *application) ClientError(w http.ResponseWriter, status int) {
 	http.Error(w, http.StatusText(status), status)
 }
